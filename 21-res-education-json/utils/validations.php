9c85b178ecce165c1581a8c92bf605b242ce5a78
sepsol <sepsol.works@gmail.com>
2025-02-23 16:00:24 -0800

    Reordering form validations

--- 21-res-education-json/utils/validations.php
+++ 21-res-education-json/utils/validations.php
@@ -28,17 +28,6 @@ function validate_profile() {
   }
 
   for ($i = 1; $i <= 9; $i++) {
-    if (!isset($_POST["pos_year_$i"])) continue;
-    if (!isset($_POST["pos_desc_$i"])) continue;
-    $year = $_POST["pos_year_$i"];
-    $desc = $_POST["pos_desc_$i"];
-
-    if (strlen($year) < 1 || strlen($desc) < 1) {
-      throw new Exception('All fields are required');
-    }
-  }
-
-  for ($i = 1; $i <= 9; $i++) {
     if (!isset($_POST["edu_year_$i"])) continue;
     if (!isset($_POST["edu_school_$i"])) continue;
     $year = $_POST["edu_year_$i"];
@@ -49,16 +38,19 @@ function validate_profile() {
     }
   }
 
-  if (!strpos($_POST['email'], '@')) {
-    throw new Exception('Email must have an at-sign (@)');
-  }
-
   for ($i = 1; $i <= 9; $i++) {
     if (!isset($_POST["pos_year_$i"])) continue;
+    if (!isset($_POST["pos_desc_$i"])) continue;
     $year = $_POST["pos_year_$i"];
-    if (!is_numeric($year)) {
-      throw new Exception('Position year must be numeric');
+    $desc = $_POST["pos_desc_$i"];
+
+    if (strlen($year) < 1 || strlen($desc) < 1) {
+      throw new Exception('All fields are required');
     }
+  }
+
+  if (!strpos($_POST['email'], '@')) {
+    throw new Exception('Email must have an at-sign (@)');
   }
 
   for ($i = 1; $i <= 9; $i++) {
@@ -66,6 +58,14 @@ function validate_profile() {
     $year = $_POST["edu_year_$i"];
     if (!is_numeric($year)) {
       throw new Exception('Education year must be numeric');
+    }
+  }
+  
+  for ($i = 1; $i <= 9; $i++) {
+    if (!isset($_POST["pos_year_$i"])) continue;
+    $year = $_POST["pos_year_$i"];
+    if (!is_numeric($year)) {
+      throw new Exception('Position year must be numeric');
     }
   }
 
